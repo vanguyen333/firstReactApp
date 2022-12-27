6d23477e65dfc59c59aec4597142c06f0fb125ff
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 18:56:08 -0500

    used react fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 
 const App = () => {
-  const name = "John";
-  const isNameShowing = true;
+  const name = null;
+
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : "somebody else!"}</h1>
+      <h1>Hello, {2 + 2}!</h1>
+      {name ? (
+        <>test</>
+      ) : (
+        <>
+          <h1>test</h1>
+          <h2>There is no name</h2>
+        </>
+      )}
     </div>
   );
 };
