71e461d1c3c8499510e96f9b877d28052295ea20
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 18:44:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const isNameShowing = true;
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : "someone!"}</h1>
+      <h1>Hello, {isNameShowing ? name : "somebody!"}</h1>
     </div>
   );
 };
