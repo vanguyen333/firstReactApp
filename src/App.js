d70be2aba4873ba3c6109c6c26b9053e5e0d66d5
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 19:39:56 -0500

    decrease, increase by 1 using React State

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  );
-};
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <Person name={"John"} lastName={"Doe"} age={30} />
-      <Person name="marry" lastName={"Doe"} age={2 + 2} />
-      <Person />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
 };
