661783ff4812fad7dc66998430935797cccffe3c
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 19:11:08 -0500

    add props.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-      <h1>Name: John</h1>
-      <h2>Last Name: Doe</h2>
-      <h2>Age: 30</h2>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   );
 };
@@ -13,7 +13,7 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-      <Person />
+      <Person name={"John"} lastName={"Doe"} age={30} />
       <Person />
       <Person />
     </div>
