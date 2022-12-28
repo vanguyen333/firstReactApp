071f44faad36c16be5fbe6fef1127385d08c4696
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 19:54:28 -0500

    used state and effect hooks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    alert("You've changed the counter to " + counter);
+    alert("You have changed the counter to " + counter);
   }, [counter]);
 
   return (
