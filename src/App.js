3406f5dcd0bc4bd73d7073e05babe59cd9e92d27
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-27 21:32:54 -0500

    recreate src folder with index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You have changed the counter to " + counter);
-  }, [counter]);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-    </div>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
